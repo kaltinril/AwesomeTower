3404f0ea6f8de07c060e2d285d328e81203fc57f
kaltinril <jeremy.swartwood@gmail.com>
2014-07-01 22:15:17 -0800

    ok

---
+++ android/src/com/twojeremys/awesometower/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.twojeremys.awesometower.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.twojeremys.awesometower.AwesomeTower;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new AwesomeTower(), config);
+	}
+}
---
+++ desktop/src/com/twojeremys/awesometower/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.twojeremys.awesometower.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.twojeremys.awesometower.AwesomeTower;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new AwesomeTower(), config);
+	}
+}
