3a744834dfe6c0622a8af162498a719a4c7eedc2
kaltinril <jeremy.swartwood@gmail.com>
2014-07-13 17:54:38 -0800

    Removed call in DefaultMenuActionListner for NewGame, since this is now handled by an Annonymous class inside MainMenuScreen.  Removed and organized imports.

--- core/src/com/twojeremys/awesometower/gamefile/GameSave.java
+++ core/src/com/twojeremys/awesometower/gamefile/GameSave.java
@@ -1,9 +1,5 @@
 package com.twojeremys.awesometower.gamefile;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.files.FileHandle;
-import com.badlogic.gdx.utils.Base64Coder;
-import com.badlogic.gdx.utils.Json;
 import com.twojeremys.awesometower.tileengine.Tile;
 
 //Create a class to contain references to only the data needed to save and restart games.
